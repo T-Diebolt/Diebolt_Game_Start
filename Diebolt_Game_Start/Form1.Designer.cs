99be31ff022fa00870d7151172eb793e1c450185
T-Diebolt <theodieb542@outlook.com>
2022-02-09 14:04:19 -0500

    Add project files.

---
+++ Diebolt_Game_Start/Form1.Designer.cs
@@ -0,0 +1,99 @@
+﻿namespace Diebolt_Game_Start
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.gameStarting = new System.Windows.Forms.Label();
+            this.startButton = new System.Windows.Forms.Button();
+            this.errorMessage = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // gameStarting
+            // 
+            this.gameStarting.Font = new System.Drawing.Font("Castellar", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.gameStarting.ForeColor = System.Drawing.Color.White;
+            this.gameStarting.Location = new System.Drawing.Point(12, 253);
+            this.gameStarting.Name = "gameStarting";
+            this.gameStarting.Size = new System.Drawing.Size(386, 22);
+            this.gameStarting.TabIndex = 0;
+            this.gameStarting.TextAlign = System.Drawing.ContentAlignment.TopCenter;
+            this.gameStarting.Click += new System.EventHandler(this.gameStart_Click);
+            // 
+            // startButton
+            // 
+            this.startButton.BackColor = System.Drawing.Color.Black;
+            this.startButton.Cursor = System.Windows.Forms.Cursors.Default;
+            this.startButton.FlatAppearance.BorderColor = System.Drawing.Color.FromArgb(((int)(((byte)(224)))), ((int)(((byte)(224)))), ((int)(((byte)(224)))));
+            this.startButton.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.startButton.Font = new System.Drawing.Font("Castellar", 24F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.startButton.ForeColor = System.Drawing.Color.LightGray;
+            this.startButton.Location = new System.Drawing.Point(99, 189);
+            this.startButton.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
+            this.startButton.Name = "startButton";
+            this.startButton.Size = new System.Drawing.Size(213, 53);
+            this.startButton.TabIndex = 1;
+            this.startButton.Text = "START";
+            this.startButton.UseVisualStyleBackColor = false;
+            this.startButton.Click += new System.EventHandler(this.startButton_Click);
+            // 
+            // errorMessage
+            // 
+            this.errorMessage.BackColor = System.Drawing.Color.Black;
+            this.errorMessage.Font = new System.Drawing.Font("Lucida Sans Typewriter", 16.2F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.errorMessage.Location = new System.Drawing.Point(12, 204);
+            this.errorMessage.Name = "errorMessage";
+            this.errorMessage.Size = new System.Drawing.Size(386, 38);
+            this.errorMessage.TabIndex = 2;
+            this.errorMessage.Text = "ERROR";
+            this.errorMessage.TextAlign = System.Drawing.ContentAlignment.TopCenter;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.Black;
+            this.ClientSize = new System.Drawing.Size(410, 511);
+            this.Controls.Add(this.startButton);
+            this.Controls.Add(this.errorMessage);
+            this.Controls.Add(this.gameStarting);
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Name = "Form1";
+            this.Text = "Gaming Gamer";
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label gameStarting;
+        private System.Windows.Forms.Button startButton;
+        private System.Windows.Forms.Label errorMessage;
+    }
+}
+
---
+++ Diebolt_Game_Start/Form1.cs
@@ -0,0 +1,75 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Threading;
+using System.Media;
+
+
+namespace Diebolt_Game_Start
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void gameStart_Click(object sender, EventArgs e)
+        {
+           
+
+
+
+        }
+
+        private void startButton_Click(object sender, EventArgs e)
+        {
+            //Giving new sound inputs
+            SoundPlayer Beep = new SoundPlayer(Properties.Resources.Beep);
+            SoundPlayer Error = new SoundPlayer(Properties.Resources.Error);
+
+            Thread.Sleep(10);
+
+            //Make button disappear
+            startButton.Visible = false;
+
+            //Game start countdown
+            gameStarting.Text = "GAME STARTING: 5";
+            Beep.Play();
+            Refresh();
+            Thread.Sleep(1000);
+            
+            gameStarting.Text = "GAME STARTING: 4";
+            Beep.Play();
+            Refresh();
+            Thread.Sleep(1000);
+
+            gameStarting.Text = "GAME STARTING: 3";
+            Beep.Play();
+            Refresh();
+            Thread.Sleep(1000);
+
+            gameStarting.Text = "GAME STARTING: 2";
+            Beep.Play();
+            Refresh();
+            Thread.Sleep(50);
+
+            gameStarting.Visible = false;
+
+            //Error Screen
+            Thread.Sleep(100);
+            Error.PlayLooping();
+            BackColor = Color.Blue;
+            errorMessage.BackColor = Color.Blue;
+            errorMessage.ForeColor = Color.White;
+
+
+        }
+    }
+}
